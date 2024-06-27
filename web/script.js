ce83e77a0e7a45baba5672354f7bf15972d554ff
mouise1111 <mouise1111@gmail.com>
2024-06-27 12:34:54 +0200

    heatmap displays colors based on entries

--- web/script.js
+++ web/script.js
@@ -1,37 +1,35 @@
 const cal = new CalHeatmap();
+
+var data = [
+  { date: '2024-06-25', habit: 0 },
+  { date: '2024-06-26', habit: 1 },
+  { date: '2024-06-27', habit: 1 },
+  { date: '2024-06-27', habit: 1 },
+  { date: '2024-06-28', habit: 1 },
+  { date: '2024-06-28', habit: 1 },
+  { date: '2024-06-28', habit: 1 },
+  { date: '2024-06-28', habit: 1 },
+  { date: '2024-06-28', habit: 1 },
+];
+
 cal.paint({
-  range: "1",
-  // data: { source: data },
-  itemSelector: "#cal-heatmap-year",
-  domain: { type: 'year', sort: 'asc' },
-  subDomain: { type: 'day' },
-  theme: "dark",
-  // date: { timezone: 'Europe/Brussels' },
-  // dinamicDimension: false
-});
-const data = [
-        { date: '2024-06-21', value: 3 },
-        { date: '2024-06-22', value: 6 },
-        { date: '2024-06-23', value: 8 }
-      ];
-const cal2 = new CalHeatmap();
-cal2.paint({
   range: 1,
-  itemSelector: "#cal-heatmap",
-  domain: { type: 'month', sort: 'asc' },
+  itemSelector: "#cal-heatmap-year",
+  domain: { type: 'year' },
   subDomain: { type: 'day' },
-  theme: "dark",
+  theme: 'dark',
   data: {
     source: data,
-    x: 'date',
-    y: 'value'
-  }
+    type: 'json', // Specify the type of data source
+    x: 'date', // Property name for the date
+    y: 'habit', // Property name for the value
+    groupY: 'sum'
+  },
+  scale: {
+    color: {
+      scheme: 'RdYlGn',
+      type: 'linear',
+      domain: [0, 5], // Domain adjusted for binary data
+    },
+  },
 });
-// cal2.paint({
-//   itemSelector: "#cal-heatmap-month",
-//   range: '1',
-//   domain: { type: 'month', sort: 'asc' },
-//   subDomain: { type: 'day' },
-//   theme: "dark",
-//   // dinamicDimension: false
-// });
