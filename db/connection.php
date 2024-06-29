e1c99bb2149aca44da1383a117aecfb97667c086
mouise1111 <you@example.com>
2024-06-29 17:30:23 +0200

    added environment variables

--- db/connection.php
+++ db/connection.php
@@ -1,9 +1,9 @@
 <?php
-$servername = "127.0.0.1";
-$username = "habits";
-$database = "habits";
-$password = "habits";
-$port = 3306;
+$servername = getenv('DB_SERVER') ?: '127.0.0.1';
+$username = getenv('DB_USER') ?: 'habits';
+$database = getenv('DB_NAME') ?: 'habits';
+$password = getenv('DB_PASS') ?: 'habits';
+$port = getenv('DB_PORT') ?: 3306;
 
 $conn = new mysqli($servername, $username, $password, $database, $port);
 
